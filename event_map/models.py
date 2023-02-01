53bd2cd029a545398a20982d2e7f11daa8aee3b8
Yelnar Muratbekov <flyingbird.kz>
2023-02-01 19:31:14 +0600

    added point on map, added polygons for departments

--- event_map/models.py
+++ event_map/models.py
@@ -1,16 +1,74 @@
 from django.db import models
 
 
-class FireHydrant(models.Model):
-    user_comments = models.TextField(max_length=100, verbose_name='Комментарии')
-    latitude = models.DecimalField(max_digits=15, decimal_places=15, verbose_name='Широта')
-    longitude = models.DecimalField(max_digits=15, decimal_places=15, verbose_name='Долгота')
-    raw_address = models.TextField(max_length=100, verbose_name='Адрес')
+class Hydrants(models.Model):
+    created_date = models.DateTimeField(verbose_name="Дата и время добавления марки", default=None)
+    user_comments = models.TextField(max_length=100, verbose_name='Комментарии', default='Комментарии')
+    latitude = models.FloatField(verbose_name='Широта', default=None)
+    longitude = models.FloatField(verbose_name='Долгота', default=None)
+    raw_address = models.TextField(max_length=100, verbose_name='Адрес', default=None)
     working = models.BooleanField(default=True)
 
     class Meta:
         verbose_name_plural = "Гидранты города"
         verbose_name = "Гидрант"
 
-    def __str__(self):
-        return self.description
+
+class FireHistory(models.Model):
+    event_date = models.DateTimeField(verbose_name="Дата и время проишествия", default=None)
+    user_comments = models.TextField(max_length=100, verbose_name='Комментарии', default='Комментарии')
+    latitude = models.FloatField(verbose_name='Широта', default=None)
+    longitude = models.FloatField(verbose_name='Долгота', default=None)
+    raw_address = models.TextField(max_length=100, verbose_name='Адрес', default=None)
+
+    class Meta:
+        verbose_name_plural = "История пожаров"
+        verbose_name = "Пожар"
+
+
+class SecurePlaces(models.Model):
+    created_date = models.DateTimeField(verbose_name="Дата и время добавления марки", default=None)
+    user_comments = models.TextField(max_length=100, verbose_name='Комментарии', default='Комментарии')
+    latitude = models.FloatField(verbose_name='Широта', default=None)
+    longitude = models.FloatField(verbose_name='Долгота', default=None)
+    raw_address = models.TextField(max_length=100, verbose_name='Адрес', default=None)
+
+    class Meta:
+        verbose_name_plural = "Объекты Повышеной Безопасности"
+        verbose_name = "ОПБ"
+
+
+class Polygon_1_Coordinates(models.Model):
+    latitude = models.FloatField(verbose_name='Широта', default=None)
+    longitude = models.FloatField(verbose_name='Долгота', default=None)
+
+    class Meta:
+        verbose_name_plural = "Область вызова СПЧ-1"
+        verbose_name = "крайняя кордината желтой области СПЧ-1"
+
+
+class Polygon_2_Coordinates(models.Model):
+    latitude = models.FloatField(verbose_name='Широта', default=None)
+    longitude = models.FloatField(verbose_name='Долгота', default=None)
+
+    class Meta:
+        verbose_name_plural = "Область вызова СПЧ-2"
+        verbose_name = "крайняя кордината желтой области СПЧ-2"
+
+
+class Polygon_3_Coordinates(models.Model):
+    latitude = models.FloatField(verbose_name='Широта', default=None)
+    longitude = models.FloatField(verbose_name='Долгота', default=None)
+
+    class Meta:
+        verbose_name_plural = "Область вызова СПЧ-3"
+        verbose_name = "крайняя кордината желтой области СПЧ-3"
+
+
+class Polygon_4_Coordinates(models.Model):
+    latitude = models.FloatField(verbose_name='Широта', default=None)
+    longitude = models.FloatField(verbose_name='Долгота', default=None)
+
+    class Meta:
+        verbose_name_plural = "Область вызова СПЧ-4"
+        verbose_name = "крайняя кордината желтой области СПЧ-4"
--- event_map/views.py
+++ event_map/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import datetime
+from .models import Hydrants, FireHistory, SecurePlaces, \
+                        Polygon_1_Coordinates, \
+                        Polygon_2_Coordinates,\
+                        Polygon_3_Coordinates,\
+                        Polygon_4_Coordinates
 import json
 
 
@@ -17,19 +22,216 @@ def new_point(request):
         now = datetime.now()
         request_time_string = now.strftime("%Y-%m-%d %H:%M:%S")
         point_type = request.POST.get('point_type')
-        point_lat = request.POST.get('lat')
-        point_long = request.POST.get('long')
+        point_lat = float(request.POST.get('lat'))
+        point_long = float(request.POST.get('long'))
         point_address = request.POST.get('address')
-        point_comment = request.POST.get('user_comments')
+        user_comments = request.POST.get('user_comments')
         if point_type == 'fire':
-            print('point_type: ', point_type)
+            FireHistory.objects.create(
+                event_date=now,
+                user_comments=user_comments,
+                latitude=point_lat,
+                longitude=point_long,
+                raw_address=point_address,
+            )
         if point_type == 'hydrant':
-            print('point_type: ', point_type)
+            Hydrants.objects.create(
+                created_date=now,
+                user_comments=user_comments,
+                latitude=point_lat,
+                longitude=point_long,
+                raw_address=point_address,
+                working=True,
+                )
         if point_type == 'secure_place':
-            print('point_type: ', point_type)
-        print('request_time_string: ', request_time_string)
-        print('point_lat: ', point_lat)
-        print('point_long: ', point_long)
-        print('point_address: ', point_address)
-        print('point_comment: ', point_comment)
+            SecurePlaces.objects.create(
+                created_date=now,
+                user_comments=user_comments,
+                latitude=point_lat,
+                longitude=point_long,
+                raw_address=point_address,
+            )
     return HttpResponse('new point request was succesfully')
+
+
+def history_points(request):
+    #FireHistory.objects.all().delete()
+    #Hydrants.objects.all().delete()
+    #SecurePlaces.objects.all().delete()
+    if (request.method == 'POST') and ('type_of_points' in request.POST):
+        requested_type = request.POST.get('type_of_points')
+        if requested_type == 'all':
+            fire_response_list = []
+            id_ = 0
+            fire_event_list = FireHistory.objects.all()
+            for fire_point in fire_event_list:
+                fire_point_dict = {"type": "Feature",
+                                   "id": id_,
+                                   "geometry": {"type": "Point",
+                                                "coordinates": [fire_point.latitude, fire_point.longitude]},
+                                   "properties": {"balloonContentHeader": 'Пожар',
+                                                  "balloonContentBody": f"<b>Адрес:</b>  {fire_point.raw_address} <br/> <b>Примечание: </b>  {fire_point.user_comments}",
+                                                   "balloonContentFooter": f"<b>Дата:</b>{fire_point.event_date}",
+                                                   "hintContent": 'Подробности метки пожар',
+                                                   },
+                                   "options": {"iconLayout": 'default#image',
+                                               "iconImageHref": "http://localhost/static/images/fire.png"
+                                                },
+                                    }
+                fire_response_list.append(fire_point_dict)
+                id_ += 1
+
+            hydrant_response_list = []
+            hydrant_event_list = Hydrants.objects.all()
+            for hydrant_point in hydrant_event_list:
+                hydrant_point_dict = {"type": "Feature",
+                                   "id": id_,
+                                   "geometry": {"type": "Point",
+                                                "coordinates": [hydrant_point.latitude, hydrant_point.longitude]},
+                                   "properties": {"balloonContentHeader": 'Гидрант',
+                                                  "balloonContentBody": f"<b>Адрес:</b>  {hydrant_point.raw_address} <br/> <b>Примечание: </b>  {hydrant_point.user_comments}",
+                                                   "balloonContentFooter": f"<b>Дата:</b>{hydrant_point.created_date}",
+                                                   "hintContent": 'Подробности метки Гидрант',
+                                                   },
+                                   "options": {"iconLayout": 'default#image',
+                                               "iconImageHref": "http://localhost/static/images/hydrant1.png"
+                                                },
+                                    }
+                hydrant_response_list.append(hydrant_point_dict)
+                id_ += 1
+
+            secure_response_list = []
+            secure_event_list = SecurePlaces.objects.all()
+            for secure_point in secure_event_list:
+                secure_point_dict = {"type": "Feature",
+                                      "id": id_,
+                                      "geometry": {"type": "Point",
+                                                   "coordinates": [secure_point.latitude, secure_point.longitude]},
+                                      "properties": {"balloonContentHeader": 'Объект повыщеной степени риска',
+                                                     "balloonContentBody": f"<b>Адрес:</b>  {secure_point.raw_address} <br/> <b>Примечание: </b>  {secure_point.user_comments}",
+                                                     "balloonContentFooter": f"<b>Дата:</b>{secure_point.created_date}",
+                                                     "hintContent": 'Подробности метки Гидрант',
+                                                     },
+                                      "options": {"iconLayout": 'default#image',
+                                                  "iconImageHref": "http://localhost/static/images/home.png"
+                                                  },
+                                      }
+                secure_response_list.append(secure_point_dict)
+                id_ += 1
+
+            hydrant_fire_secure_event_dict = {'type': "FeatureCollection",
+                                      "features": hydrant_response_list+fire_response_list+secure_response_list}
+            hydrant_event_secure_response = json.dumps(hydrant_fire_secure_event_dict)
+            return HttpResponse(hydrant_event_secure_response, content_type="application/json")
+
+
+def get_polygon_1_points(request):
+    if request.method == 'POST' and 'update_polygon_1_points' in request.POST:
+        Polygon_1_Coordinates.objects.all().delete()
+        print('len-', len(request.POST))
+        for id_ in range(1, len(request.POST)):
+            point = request.POST.get(f'{id_}')
+            point_lat = float(point.split(',')[0])
+            point_long = float(point.split(',')[1])
+            print(id_, '-', point_lat, point_long)
+            Polygon_1_Coordinates.objects.create(
+                latitude=point_lat,
+                longitude=point_long,
+                )
+        return HttpResponse('get_polygon_1_points success')
+    if request.method == 'GET':
+        polygon_1_coordinates = Polygon_1_Coordinates.objects.all()
+        polygon_1_coordinates_dict = {}
+        id_ = 0
+        for point in polygon_1_coordinates:
+            latitude = point.latitude
+            longitude = point.longitude
+            polygon_1_coordinates_dict[id_] = {'latitude': latitude,
+                                               'longitude': longitude}
+            id_ += 1
+        print(polygon_1_coordinates_dict)
+        polygon_1_coordinates_json = json.dumps(polygon_1_coordinates_dict)
+        return HttpResponse(polygon_1_coordinates_json, content_type="application/json")
+
+
+def get_polygon_2_points(request):
+    if request.method == 'POST' and 'update_polygon_2_points' in request.POST:
+        Polygon_2_Coordinates.objects.all().delete()
+        for id_ in range(1, len(request.POST)):
+            point = request.POST.get(f'{id_}')
+            point_lat = float(point.split(',')[0])
+            point_long = float(point.split(',')[1])
+            Polygon_2_Coordinates.objects.create(
+                latitude=point_lat,
+                longitude=point_long,
+                )
+        return HttpResponse('get_polygon_2_points success')
+    if request.method == 'GET':
+        polygon_2_coordinates = Polygon_2_Coordinates.objects.all()
+        polygon_2_coordinates_dict = {}
+        id_ = 0
+        for point in polygon_2_coordinates:
+            latitude = point.latitude
+            longitude = point.longitude
+            polygon_2_coordinates_dict[id_] = {'latitude': latitude,
+                                               'longitude': longitude}
+            id_ += 1
+        print(polygon_2_coordinates_dict)
+        polygon_2_coordinates_json = json.dumps(polygon_2_coordinates_dict)
+        return HttpResponse(polygon_2_coordinates_json, content_type="application/json")
+    return HttpResponse('empty')
+
+
+def get_polygon_3_points(request):
+    if request.method == 'POST' and 'update_polygon_3_points' in request.POST:
+        Polygon_3_Coordinates.objects.all().delete()
+        for id_ in range(1, len(request.POST)):
+            point = request.POST.get(f'{id_}')
+            point_lat = float(point.split(',')[0])
+            point_long = float(point.split(',')[1])
+            Polygon_3_Coordinates.objects.create(
+                latitude=point_lat,
+                longitude=point_long,
+                )
+            print(point_long, point_long)
+        return HttpResponse('get_polygon_3_points success')
+    if request.method == 'GET':
+        polygon_3_coordinates = Polygon_3_Coordinates.objects.all()
+        polygon_3_coordinates_dict = {}
+        id_ = 0
+        for point in polygon_3_coordinates:
+            latitude = point.latitude
+            longitude = point.longitude
+            polygon_3_coordinates_dict[id_] = {'latitude': latitude,
+                                               'longitude': longitude}
+            id_ += 1
+        polygon_3_coordinates_json = json.dumps(polygon_3_coordinates_dict)
+        return HttpResponse(polygon_3_coordinates_json, content_type="application/json")
+    return HttpResponse('empty')
+
+
+def get_polygon_4_points(request):
+    if request.method == 'POST' and 'update_polygon_4_points' in request.POST:
+        Polygon_4_Coordinates.objects.all().delete()
+        for id_ in range(1, len(request.POST)):
+            point = request.POST.get(f'{id_}')
+            point_lat = float(point.split(',')[0])
+            point_long = float(point.split(',')[1])
+            Polygon_4_Coordinates.objects.create(
+                latitude=point_lat,
+                longitude=point_long,
+                )
+        return HttpResponse('get_polygon_4_points success')
+    if request.method == 'GET':
+        polygon_4_coordinates = Polygon_4_Coordinates.objects.all()
+        polygon_4_coordinates_dict = {}
+        id_ = 0
+        for point in polygon_4_coordinates:
+            latitude = point.latitude
+            longitude = point.longitude
+            polygon_4_coordinates_dict[id_] = {'latitude': latitude,
+                                               'longitude': longitude}
+            id_ += 1
+        polygon_4_coordinates_json = json.dumps(polygon_4_coordinates_dict)
+        return HttpResponse(polygon_4_coordinates_json, content_type="application/json")
+    return HttpResponse('empty')
